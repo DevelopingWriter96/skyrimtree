f32c22542bb1f6c7dbdd4029137ea07fed561665
Richard Stacey <staceyrichard1996@gmail.com>
2024-02-22 08:53:42 -0700

    Finished Pickpocket and changed data order

--- script.js
+++ script.js
@@ -1,5 +1,140 @@
 const treeData = [
     {
+        name: "Alchemist",
+        category: "Thief",
+        perkLevel: 1,
+        skillName: "Alchemy",
+        skillCost: 0,
+        prerequisites: "none",
+        skillDescription: "Potions and poisons you make are 20% stronger."
+    },
+    {
+        name: "Alchemist",
+        category: "Thief",
+        perkLevel: 2,
+        skillName: "Alchemy",
+        skillCost: 20,
+        prerequisites: "none",
+        skillDescription: "Potions and poisons you make are 40% stronger."
+    },
+    {
+        name: "Alchemist",
+        category: "Thief",
+        perkLevel: 3,
+        skillName: "Alchemy",
+        skillCost: 40,
+        prerequisites: "none",
+        skillDescription: "Potions and poisons you make are 60% stronger."
+    },
+    {
+        name: "Alchemist",
+        category: "Thief",
+        perkLevel: 4,
+        skillName: "Alchemy",
+        skillCost: 60,
+        prerequisites: "none",
+        skillDescription: "Potions and poisons you make are 60% stronger."
+    },
+    {
+        name: "Alchemist",
+        category: "Thief",
+        perkLevel: 5,
+        skillName: "Alchemy",
+        skillCost: 80,
+        prerequisites: "none",
+        skillDescription: "Potions and poisons you make are twice as strong."
+    },
+    {
+        name: "Physician",
+        category: "Thief",
+        perkLevel: 1,
+        skillName: "Alchemy",
+        skillCost: 20,
+        prerequisites: "Alchemist",
+        skillDescription: "Potions you mix that restore health or stamina are 25% more powerful."
+    },
+    {
+        name: "Poisoner",
+        category: "Thief",
+        perkLevel: 1,
+        skillName: "Alchemy",
+        skillCost: 30,
+        prerequisites: "Physician",
+        skillDescription: "Poisons you mix are 25% more effective."
+    },
+    {
+        name: "Benefactor",
+        category: "Thief",
+        perkLevel: 1,
+        skillName: "Alchemy",
+        skillCost: 30,
+        prerequisites: "Physician",
+        skillDescription: "Potions you mix with beneficial effects have an additional 25% greater magnitude."
+    },
+    {
+        name: "Experimenter",
+        category: "Thief",
+        perkLevel: 1,
+        skillName: "Alchemy",
+        skillCost: 50,
+        prerequisites: "Benefactor",
+        skillDescription: "Eating an ingredient reveals the first two effects."
+    },
+    {
+        name: "Experimenter",
+        category: "Thief",
+        perkLevel: 2,
+        skillName: "Alchemy",
+        skillCost: 70,
+        prerequisites: "Benefactor",
+        skillDescription: "Eating an ingredient reveals the first three effects."
+    },
+    {
+        name: "Experimenter",
+        category: "Thief",
+        perkLevel: 3,
+        skillName: "Alchemy",
+        skillCost: 90,
+        prerequisites: "Benefactor",
+        skillDescription: "Eating an ingredient reveals the first four effects."
+    },
+    {
+        name: "Concentrated Poison",
+        category: "Thief",
+        perkLevel: 1,
+        skillName: "Alchemy",
+        skillCost: 60,
+        prerequisites: "Poisoner",
+        skillDescription: "Poisons applied to weapons last for twice as many hits."
+    },
+    {
+        name: "Green Thumb",
+        category: "Thief",
+        perkLevel: 1,
+        skillName: "Alchemy",
+        skillCost: 70,
+        prerequisites: "Concentrated Poison",
+        skillDescription: "Two ingredients are gathered from plants."
+    },
+    {
+        name: "Snakeblood",
+        category: "Thief",
+        perkLevel: 1,
+        skillName: "Alchemy",
+        skillCost: 80,
+        prerequisites: "Concentrated Poison, Experimenter",
+        skillDescription: "50% resistance to all poisons."
+    },
+    {
+        name: "Purity",
+        category: "Thief",
+        perkLevel: 1,
+        skillName: "Alchemy",
+        skillCost: 100,
+        prerequisites: "Snakeblood",
+        skillDescription: "All negative effects are removed from created potions while all positive effects are removed from created poisons."
+    },
+    {
         name: "Agile Defender",
         category: "Thief",
         perkLevel: 1,
@@ -200,157 +335,93 @@ const treeData = [
     {
         name: "Light Fingers",
         category: "Thief",
-        perkLevel: 1,
+        perkLevel: 3,
         skillName: "Pickpocket",
-        skillCost: 0,
+        skillCost: 40,
         prerequisites: "None",
         skillDescription: "Pickpocketing bonus of 20%. Item weight and value reduce pickpocketing odds (+20% per additional rank)."
     },
     {
         name: "Light Fingers",
         category: "Thief",
-        perkLevel: 3,
+        perkLevel: 4,
         skillName: "Pickpocket",
-        skillCost: 40,
+        skillCost: 60,
         prerequisites: "None",
         skillDescription: "Pickpocketing bonus of 20%. Item weight and value reduce pickpocketing odds (+20% per additional rank)."
     },
     {
-        name: "Alchemist",
-        category: "Thief",
-        perkLevel: 1,
-        skillName: "Alchemy",
-        skillCost: 0,
-        prerequisites: "none",
-        skillDescription: "Potions and poisons you make are 20% stronger."
-    },
-    {
-        name: "Alchemist",
-        category: "Thief",
-        perkLevel: 2,
-        skillName: "Alchemy",
-        skillCost: 20,
-        prerequisites: "none",
-        skillDescription: "Potions and poisons you make are 40% stronger."
-    },
-    {
-        name: "Alchemist",
-        category: "Thief",
-        perkLevel: 3,
-        skillName: "Alchemy",
-        skillCost: 40,
-        prerequisites: "none",
-        skillDescription: "Potions and poisons you make are 60% stronger."
-    },
-    {
-        name: "Alchemist",
-        category: "Thief",
-        perkLevel: 4,
-        skillName: "Alchemy",
-        skillCost: 60,
-        prerequisites: "none",
-        skillDescription: "Potions and poisons you make are 60% stronger."
-    },
-    {
-        name: "Alchemist",
+        name: "Light Fingers",
         category: "Thief",
         perkLevel: 5,
-        skillName: "Alchemy",
+        skillName: "Pickpocket",
         skillCost: 80,
-        prerequisites: "none",
-        skillDescription: "Potions and poisons you make are twice as strong."
+        prerequisites: "None",
+        skillDescription: "Pickpocketing bonus of 20%. Item weight and value reduce pickpocketing odds (+20% per additional rank)."
     },
     {
-        name: "Physician",
+        name: "Night Thief",
         category: "Thief",
         perkLevel: 1,
-        skillName: "Alchemy",
-        skillCost: 20,
-        prerequisites: "Alchemist",
-        skillDescription: "Potions you mix that restore health or stamina are 25% more powerful."
-    },
-    {
-        name: "Poisoner",
-        category: "Thief",
-        perkLevel: 1,
-        skillName: "Alchemy",
+        skillName: "Pickpocket",
         skillCost: 30,
-        prerequisites: "Physician",
-        skillDescription: "Poisons you mix are 25% more effective."
+        prerequisites: "Light Fingers",
+        skillDescription: "+25% chance to pickpocket if the target is asleep."
     },
     {
-        name: "Benefactor",
+        name: "Poisoned",
         category: "Thief",
         perkLevel: 1,
-        skillName: "Alchemy",
-        skillCost: 30,
-        prerequisites: "Physician",
-        skillDescription: "Potions you mix with beneficial effects have an additional 25% greater magnitude."
+        skillName: "Pickpocket",
+        skillCost: 40,
+        prerequisites: "Night Thief",
+        skillDescription: "Silently harm enemies by placing poisons in their pockets."
     },
     {
-        name: "Experimenter",
+        name: "Cutpurse",
         category: "Thief",
         perkLevel: 1,
-        skillName: "Alchemy",
+        skillName: "Pickpocket",
+        skillCost: 40,
+        prerequisites: "Night Thief",
+        skillDescription: "Pickpocketing gold becomes 50% easier."
+    },
+    {
+        name: "Extra Pockets",
+        category: "Thief",
+        perkLevel: 1,
+        skillName: "Pickpocket",
         skillCost: 50,
-        prerequisites: "Benefactor",
-        skillDescription: "Eating an ingredient reveals the first two effects."
+        prerequisites: "Night Thief",
+        skillDescription: "Carrying capacity is increased by 100."
     },
     {
-        name: "Experimenter",
-        category: "Thief",
-        perkLevel: 2,
-        skillName: "Alchemy",
-        skillCost: 70,
-        prerequisites: "Benefactor",
-        skillDescription: "Eating an ingredient reveals the first three effects."
-    },
-    {
-        name: "Experimenter",
-        category: "Thief",
-        perkLevel: 3,
-        skillName: "Alchemy",
-        skillCost: 90,
-        prerequisites: "Benefactor",
-        skillDescription: "Eating an ingredient reveals the first four effects."
-    },
-    {
-        name: "Concentrated Poison",
+        name: "Keymaster",
         category: "Thief",
         perkLevel: 1,
-        skillName: "Alchemy",
+        skillName: "Pickpocket",
         skillCost: 60,
-        prerequisites: "Poisoner",
-        skillDescription: "Poisons applied to weapons last for twice as many hits."
+        prerequisites: "Cutpurse",
+        skillDescription: "Pickpocketing keys almost always works."
     },
     {
-        name: "Green Thumb",
+        name: "Misdirection",
         category: "Thief",
         perkLevel: 1,
-        skillName: "Alchemy",
+        skillName: "Pickpocket",
         skillCost: 70,
-        prerequisites: "Concentrated Poison",
-        skillDescription: "Two ingredients are gathered from plants."
+        prerequisites: "Cutpurse",
+        skillDescription: "Can pickpocket equipped weapons (when hidden)."
     },
     {
-        name: "Snakeblood",
+        name: "Perfect Touch",
         category: "Thief",
         perkLevel: 1,
-        skillName: "Alchemy",
-        skillCost: 80,
-        prerequisites: "Concentrated Poison, Experimenter",
-        skillDescription: "50% resistance to all poisons."
-    },
-    {
-        name: "Purity",
-        category: "Thief",
-        perkLevel: 1,
-        skillName: "Alchemy",
+        skillName: "Pickpocket",
         skillCost: 100,
-        prerequisites: "Snakeblood",
-        skillDescription: "All negative effects are removed from created potions while all positive effects are removed from created poisons."
+        prerequisites: "Misdirection",
+        skillDescription: "Can pickpocket equipped items, clothes and armor (when hidden)."
     },
-
 ]
 
 console.log(treeData);
